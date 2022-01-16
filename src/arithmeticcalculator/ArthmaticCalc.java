1de29a1c0af6b70d3650b6206992bfff94c406eb
aishwarya8618 <97776828+aishwarya8618@users.noreply.github.com>
2022-01-16 17:21:02 +0530

    Add files via upload

---
+++ src/arithmeticcalculator/ArthmaticCalc.java
@@ -0,0 +1,25 @@
+package arithmeticcalculator;
+import java.util.Scanner;
+public class ArthmaticCalc {
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+System.out.println("eneter the two numbers");
+int firstnum=sc.nextInt();
+int secondnum= sc.nextInt();
+System.out.println("Enter the operator ");
+char op=sc.next().charAt(0);
+double Ans=0;
+switch(op){
+case '+': Ans=firstnum+secondnum;
+break;
+case '-':Ans=firstnum-secondnum;
+break;
+case '*': Ans=firstnum*secondnum;
+break;
+case '/':Ans=firstnum/secondnum;
+break;
+}
+System.out.println("the answer is " +Ans);
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module projectphase1 {
+}
